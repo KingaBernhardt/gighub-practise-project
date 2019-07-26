3d0818a33731725f740164db125a4d6bb6166f24
kinga bernhardt <kinga.bernhardt@rpharms.com>
2019-07-26 13:55:53 +0100

    Add creation of a gig

--- Gighub/Controllers/GigsController.cs
+++ Gighub/Controllers/GigsController.cs
@@ -1,5 +1,6 @@
 ﻿using Gighub.Models;
 using Gighub.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,7 @@ namespace Gighub.Controllers
             _context = new AppDbContext();
         }
         // GET: Gigs
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel()
@@ -23,6 +25,24 @@ namespace Gighub.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == artistId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var gig = new Gig()
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(String.Format("{0}, {1}", viewModel.Date, viewModel.Genre)),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
